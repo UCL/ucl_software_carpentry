b57d28c79aa9c78d016feb8602ce7cbdfdeadc30
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2013-04-03 20:11:24 +0100

    More fixes to exercises lesson 1 and 2.

--- python/02_data_structures/exercises.py
+++ python/02_data_structures/exercises.py
@@ -1,83 +1,134 @@
 # -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
+# <nbformat>2</nbformat>
 
 # <markdowncell>
 
 # # Exercise 1
-# 1. Calculate the mean of the numbers 2, 3, and 10.
+# 
+# 1. Calculate the mean of the numbers 2, 3, and 10 (using methods available in list to sum and count number of elements).
 
 # <codecell>
+
   
+
 # <markdowncell>
 
 # # Exercise 2
+# 
 # 1. Make a list with 5 things in it.
+
 # <codecell>
+
   
-# 1. Add two more things.
-# <codecell>
-   
+
 # <markdowncell>
-# # Exercise 2
-# 1. Make a dictionary whose keys are the strings "zero" through "nine" and whose values are ints 0 through 9.
+
+# 2. Add two more things.
+
 # <codecell>
+
+   
+
+# <markdowncell>
+
+# # Exercise 2
+# 
+# 1. Make a dictionary whose keys are the strings "zero" through "nine" and whose values are ints 0 through 9.
+
+# <codecell>
+
 digitsdict = {}
+
 #  Your code goes here
 
 # <markdowncell>
+
 # # Exercise 3
+# 
 # 1. Make a dictionary and experiment using different types as keys. Can containers be keys?
 
 # <codecell>
+
   
 
 # <markdowncell>
 
 # # Exercises 4
+# 
 # 1. Write an if statement that prints whether x is even or odd
-# <codecell>
-x=4
-print x
-if x < 5:
-  print "x is less than five!"
-else: 
-  print "x is not less than five!"
 
 # <codecell>
+
+x=4
+
+print x
+
+if x < 5:
+
+  print "x is less than five!"
+
+else: 
+
+  print "x is not less than five!"
 
 # <markdowncell>
 
 # # Exercises 5
+# 
 # 1. Using a loop, calculate the factorial of 42 (the product of all integers up to and including 42).
 
 # <codecell>
+
    
+
 # <markdowncell>
 
 # # Exercises 6
+# 
 # 1. Create a function that returns the factorial of a given number.
 
 # <codecell>
+
  
+
 # <markdowncell>
 
 # # Exercise 7
-# 7A. Read the file 'big_animals.txt' and print each line and the length of each line.
-# <codecell>
-
-# 7B. Read the file 'big_animals.txt' again and print each line where the number of animals sighted is greater than 10.  We'll won't output the lines with fewer than 10 animals.
-# <codecell>
-
-# 7C. Read the file 'big_animals.txt' again and return a list of tuples.
-# <codecell>
-
-# 7D. Turn the code for #7C into a function and use it on the files 'merida_animals.txt' and 'fergus_animals.txt'.
+# 
+# 7A. Read the file 'animals.txt' and print each line and the length of each line.
 
 # <codecell>
- 
+
+
 # <markdowncell>
-# # Exercise 8
-# 1.  Check your answer to Exercise 5 (the value of 42!) using the **math.gamma()** function.
+
+#  7B. Read the file 'big_animals.txt' again and print each line where the number of animals sighted is greater than 10.  We won't output the lines with fewer than 10 animals.
+
 # <codecell>
+
+
+# <markdowncell>
+
+# 7C. Read the file 'big_animals.txt' again and return a list of tuples (one per line) with animal name and number of sights.
+
+# <codecell>
+
+
+# <markdowncell>
+
+# 7D. Turn the code for 7C into a function and use it on the files 'merida_animals.txt' and 'fergus_animals.txt'. (Optional, you will learn how to define your own functions in Lesson 4)
+
+# <codecell>
+
+ 
+
+# <markdowncell>
+
+# # Exercise 8
+# 
+# 1.  Check your answer to Exercise 5 (the value of 42!) using the **math.gamma()** function.
+
+# <codecell>
+
 import math
 
