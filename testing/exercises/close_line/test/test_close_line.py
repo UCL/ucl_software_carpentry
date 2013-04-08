ce89aa52719b041cba04cadd2e0bef408e22d65d
James Hetherington <j.hetherington@ucl.ac.uk>
2013-04-08 11:51:23 +0100

    Add numpy to close line

--- testing/exercises/close_line/test/test_close_line.py
+++ testing/exercises/close_line/test/test_close_line.py
@@ -3,19 +3,24 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
 
 from close_line import \
    closest_data_to_line1,closest_data_to_line2,closest_data_to_line3,closest_data_to_line4
- 
+           
+functions_to_test=[closest_data_to_line1,closest_data_to_line2,closest_data_to_line3,closest_data_to_line4] 
+
+import numpy.testing
+
 # Here's an example test function, you should add more:
-def test_one_point_method_1():
+def test_one_point_all_methods():
     
 
-      data=[
+      data=numpy.array([
         [0.,0.] # This data scatter has one point
-        ]
+        ])
 
-      line_first_point=[1.,0.]
-      line_second_point=[0.,1.]
-
-      result=closest_data_to_line1(data,line_first_point,line_second_point)
-                                     
+      line_first_point=numpy.array([1.,0.])
+      line_second_point=numpy.array([0.,1.])
+      
+      result=closest_data_to_line2(data,line_first_point,line_second_point)
+                                    
       # Output is an index of the minimum, and then the minimum
-      assert_equal((0, [0.,0.]),result) 
+      assert_equal(0, result[0])
+      numpy.testing.assert_array_equal(result[1],numpy.array([0.,0.]))
