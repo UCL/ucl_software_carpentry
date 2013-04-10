133e1e790ced9c75f63bd05cf4e6bfa358197072
jamespjh <jamespjh@googlemail.com>
2013-04-10 17:51:14 +0200

    Fix incorrect test name

--- testing/exercises/close_line/test/test_close_line.py
+++ testing/exercises/close_line/test/test_close_line.py
@@ -17,7 +17,7 @@ def test_one_point_method1():
       line_first_point=numpy.array([1.,0.])
       line_second_point=numpy.array([0.,1.])
       
-      result=closest_data_to_line2(data,line_first_point,line_second_point)
+      result=closest_data_to_line1(data,line_first_point,line_second_point)
                                     
       # Output is an index of the minimum, and then the minimum
       assert_equal(0, result[0])
